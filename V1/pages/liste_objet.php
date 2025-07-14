fcabf9e95af577e6232c5ee3644601619fd0616c
NyAvo77 <nyavo.ando@gmail.com>
2025-07-14 11:23:52 +0300

    farany

--- V1/pages/liste_objet.php
+++ V1/pages/liste_objet.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
     <h1>Liste des objets</h1>
--- V1/pages/login.php
+++ V1/pages/login.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
     <form action="traitement_login.php" method="post">
